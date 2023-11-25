8517d04f60025eb7fc1870a26605918946ca6b83
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-25 14:29:06 -0300

    feat: Add Alt on image.

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`Image ${invoice.name}`}
                       />
                       <p>{invoice.name}</p>
                     </div>
