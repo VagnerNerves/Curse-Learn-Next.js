950111d84ffdef2234a3266c0545828fa0e27cc4
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-26 20:54:38 -0300

    feat: Install next-auth and Create Authenticate and page login.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,7 @@ import { redirect } from 'next/navigation';
 import { sql } from '@vercel/postgres';
 
 import { z } from 'zod';
+import { signIn } from '@/auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -93,8 +94,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
@@ -102,5 +101,19 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
